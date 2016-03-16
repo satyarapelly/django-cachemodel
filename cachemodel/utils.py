6fe912c7937f2f6edb860d80e4c55c828b85f1d3
Wiggins <wiggins@concentricsky.com>
2016-03-15 22:24:35 -0700

    django unicode support

--- cachemodel/utils.py
+++ cachemodel/utils.py
@@ -1,11 +1,13 @@
 from hashlib import md5
 
+from django.utils.encoding import smart_bytes
+
 
 def generate_cache_key(prefix, *args, **kwargs):
-    arg_str = ":".join(str(a) for a in args)
-    kwarg_str = ":".join("%s=%s"%(str(k), str(v)) for k,v in kwargs.items())
-    key_str = "%s::%s" % (arg_str, kwarg_str)
-    argkwarg_str = md5(key_str).hexdigest()
+    arg_str = ":".join(smart_bytes(a) for a in args)
+    kwarg_str = ":".join("{}={}".format(smart_bytes(k), smart_bytes(v)) for k, v in kwargs.items())
+    key_str = "{}::{}".format(arg_str, kwarg_str)
+    argkwarg_str = md5(smart_bytes(key_str)).hexdigest()
     if not isinstance(prefix, basestring):
         prefix = "_".join(str(a) for a in prefix)
-    return "%s__%s" % (prefix, argkwarg_str)
+    return "{}__{}".format(prefix, argkwarg_str)
